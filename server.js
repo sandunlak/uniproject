4b3ffe0b5f5b8a60cfdac9eda11087d492a354ad
sandunlak <138517951+sandunlak@users.noreply.github.com>
2024-06-30 20:14:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");  //Create variable and assign value
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070; //If 8070 not avilable assign another avilalabe port number
+
+app.use(cors());
+app.use(bodyparser.json());
+
+const URL = process.env.MONGODB_URL; //connect to mongodb
+
+mongoose.connect(URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   
+});
+
+const connection = mongoose.connection;
+connection.once("open",() => {  //Open the created connection
+    console.log("MongoDb connection success!");
+})
+
+app.listen(PORT,() => {
+    console.log(`server is up and running on port number: ${PORT}`)
+})
